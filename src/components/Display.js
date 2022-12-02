35433401a3ea1317b41a19cd8adc40237b5663de
David Zambrano <david._101@hotmail.com>
2022-12-02 15:57:08 -0500

    Refactor code with hooks

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export class Display extends React.PureComponent {
-  render() {
-    const { value } = this.props;
-    return (
-      <div className="display">
-        <p>{value}</p>
-      </div>
-    );
-  }
+export default function Display({ value }) {
+  return (
+    <div className="display">
+      <p>{value}</p>
+    </div>
+  );
 }
 
 Display.propTypes = {
   value: PropTypes.string.isRequired,
 };
-
-export default Display;
