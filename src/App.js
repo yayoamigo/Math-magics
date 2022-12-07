333d60e6c9c881c1f9907da1b4dab8564a8bf4a0
David Zambrano <david._101@hotmail.com>
2022-12-07 15:14:22 -0500

    Implement snapshot test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React, { PureComponent } from 'react';
-import {
-  Routes,
-  Route,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import Home from './pages/Home';
 import Navbar from './pages/Navbar';
@@ -11,14 +8,16 @@ import Quote from './pages/Quotes';
 export default class App extends PureComponent {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="calculator" element={<Calculator />} />
-          <Route path="quote" element={<Quote />} />
-        </Routes>
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="calculator" element={<Calculator />} />
+            <Route path="quote" element={<Quote />} />
+          </Routes>
+        </div>
+      </Router>
     );
   }
 }
